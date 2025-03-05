0cd86f5c20cb9a96c8522b587bfee5d7c71dbe1b
Andreas242 <andreas.ahlgren@soprasteria.com>
2025-03-05 10:00:31 +0100

    Added scaffolding to the project

--- src/app/directives/format-phone-number.directive.ts
+++ src/app/directives/format-phone-number.directive.ts
@@ -1,27 +1,17 @@
-import { Directive, ElementRef, Renderer2, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appFormatPhoneNumber]'
 })
 export class FormatPhoneNumberDirective implements OnChanges {
 
-  @Input() appFormatPhoneNumber: string = ''; // Receives the dynamic data
-
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['appFormatPhoneNumber']) {
-      this.formatPhoneNumber();
-    }
+  // implement ngOnChanges
   }
 
   private formatPhoneNumber(): void {
-    if (this.appFormatPhoneNumber) {
-      // Remove everything after and including 'x' and replace '.' with '-'
-      const formattedValue = this.appFormatPhoneNumber.split('x')[0].replace(/\./g, '-');
-
-      // Set the formatted value back into the element
-      this.renderer.setProperty(this.el.nativeElement, 'textContent', formattedValue);
-    }
+    // add logic to format number here
   }
 }
